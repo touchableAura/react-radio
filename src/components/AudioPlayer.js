0cfed4e7ad4bf67c50d88c5ae181162f0c402c15
touchableAura <evancamerondev@proton.me>
2024-01-03 16:03:51 -0400

    width changes

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -15,15 +15,15 @@ const AudioPlayer = () => {
     { id: 3, title: "CJLO", url: "http://rosetta.shoutca.st:8883/stream", about:"1690 AM - CJLO is based at Concordia University in Montreal, Quebec" }, // CJLO 1690 Concordia University, Montreal, QC
     { id: 4, title: "CKUT", url: "https://delray.ckut.ca:8001/ckut-live-128", about:"90.3 FM - CKUT is based at McGill University in Montreal, Quebec"}, //	CKUT-FM 90.3 McGill University - Montreal, QC
     {	id: 5, title: "CILU", url:"http://luradio-server.lakeheadu.ca:8000/stereo128", about:"102.7 FM - CILU is based at Lakehead University in Thunder Bay, Ontario"}, // CILU 102.7 "LU Radio" Lakehead University - Thunder Bay, ON
-    { id: 6, title: "CKMS", url:"https://studio.radiowaterloo.ca:8443/radiowaterloo", about:"12.7 FM - CKMS is based at the University of Waterloo in Waterloo, Ontario"}, // 	CKMS-FM Radio Waterloo DI
-    { id: 7,  title: "CKUW", url: "http://shout.mtl.gameservers.com:9025/;?type=http&nocache=10037", about: "95.9 FM - CKUW is based at the University of Winnipeg in Winnipeg, Manitoba"}, // CKUW 95.9 University of Winnipeg, MB
+    { id: 6, title: "CKMS", url:"https://studio.radiowaterloo.ca:8443/radiowaterloo", about:"12.7 FM - CKMS is based at the University of Waterloo in Ontario"}, // 	CKMS-FM Radio Waterloo DI
+    { id: 7,  title: "CKUW", url: "http://shout.mtl.gameservers.com:9025/;?type=http&nocache=10037", about: "95.9 FM - CKUW is based at the University of Winnipeg in Manitoba"}, // CKUW 95.9 University of Winnipeg, MB
     { id: 8, title: "CJSR", url: "http://cjsr.streamon.fm:8000/CJSR-24k.aac", about: "88.5 FM - CJSR is based at the University of Alberta in Edmonton, Alberta"}, // CJSR 88.5 University of Alberta, Edmonton, AB
-    { id: 9, title: "CFUV", url: "http://cfuv.streamon.fm:8000/CFUV-64k.aac",about:"101.9 FM - CFUV is based at the University of Victoria in Victoria, British Columbia" }, // CFUV 101.9 University of Victoria, BC
+    { id: 9, title: "CFUV", url: "http://cfuv.streamon.fm:8000/CFUV-64k.aac",about:"101.9 FM - CFUV is based at the University of Victoria in British Columbia" }, // CFUV 101.9 University of Victoria, BC
     { id: 10, title: "WNYC", url: "http://fm939.wnyc.org/wnycfm.aac", about:"93.9 FM - WNYC is independent radio based in New York City ", }, // NY public radio
-    { id: 11, title: "WFMU", url: "http://stream0.wfmu.org/do-or-diy",about:"91.1 FM - WFMU is independent radio based in New Jersey.", }, // NJ indie radio
-    { id: 12, title: "KEXP", url: "http://live-mp3-128.kexp.org/kexp128.mp3", about:"KEXP is based at the University of Washington." }, // University of Washington
-    { id: 13, title: "NTS1", url: "http://stream-relay-geo.ntslive.net/stream", about:"NTS is a global radio platform broadcasting music from over 50 cities around the globe, live 24/7." }, // global channel 1
-    { id: 14, title: "NTS2", url: "http://stream-relay-geo.ntslive.net/stream2", about:"NTS is a global radio platform broadcasting music from over 50 cities around the globe, live 24/7."} // global channel 2
+    { id: 11, title: "WFMU", url: "http://stream0.wfmu.org/do-or-diy",about:"91.1 FM - WFMU is independent radio based in New Jersey", }, // NJ indie radio
+    { id: 12, title: "KEXP", url: "http://live-mp3-128.kexp.org/kexp128.mp3", about:"KEXP is based at the University of Washington" }, // University of Washington
+    { id: 13, title: "NTS1", url: "http://stream-relay-geo.ntslive.net/stream", about:"NTS is a global radio platform broadcasting music from over 50 cities" }, // global channel 1
+    { id: 14, title: "NTS2", url: "http://stream-relay-geo.ntslive.net/stream2", about:"NTS is a global radio platform broadcasting music from over 50 cities"} // global channel 2
     // Add more streams as needed
   ];
 
